5a532a8db266f7f1bfd43a1066c9bba3d0182e0d
john-reiner <johnreiner1990@gmail.com>
2020-08-19 16:10:24 -0400

    added a utils directory and moved the geocode function to that

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = `http://api.weatherstack.com/current?access_key=79a76f2346853bce29476e23b04cc23a&query=74.1996,40.33&units=f`
 
@@ -15,19 +16,24 @@ const request = require('request')
 
 
 
-const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12.json?types=country&access_token=pk.eyJ1Ijoiam9obnJlaW5lciIsImEiOiJja2RhcGwyejUwZ2p2MnhqcXVzOTNnenRuIn0.7R9tDcoZqcUASFvjcfUqUg&limit=1'
+// const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/12.json?types=country&access_token=pk.eyJ1Ijoiam9obnJlaW5lciIsImEiOiJja2RhcGwyejUwZ2p2MnhqcXVzOTNnenRuIn0.7R9tDcoZqcUASFvjcfUqUg&limit=1'
 
-request({url: url2, json: true}, (error, response) => {
+// request({url: url2, json: true}, (error, response) => {
 
-    if (error) {
-        console.log('Unable to connect to location services')
-    } else if (response.body.message || response.body.features.length === 0){
-        console.log("unable to locate ")
-    } else {
-        let latitude = response.body.features[0].center[0]
-        let longitude = response.body.features[0].center[1]
-        console.log(latitude, longitude)        
-    }
+//     if (error) {
+//         console.log('Unable to connect to location services')
+//     } else if (response.body.message || response.body.features.length === 0){
+//         console.log("unable to locate ")
+//     } else {
+//         let latitude = response.body.features[0].center[0]
+//         let longitude = response.body.features[0].center[1]
+//         console.log(latitude, longitude)        
+//     }
 
+// })
+
+
+geocode('Boston', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
-
